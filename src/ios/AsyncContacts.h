91d347922ef17c0228cc3a3bc4bc8089ec1165fb
mrameezraja <mrameezraja@gmail.com>
2015-02-05 15:16:27 +0500

    minor fixes

---
+++ src/ios/AsyncContacts.h
@@ -0,0 +1,14 @@
+
+#import <Foundation/Foundation.h>
+#import <AddressBook/ABAddressBook.h>
+#import <AddressBookUI/AddressBookUI.h>
+#import <Cordova/CDVPlugin.h>
+
+@interface AsyncContacts : CDVPlugin <ABNewPersonViewControllerDelegate,
+                         ABPersonViewControllerDelegate,
+                         ABPeoplePickerNavigationControllerDelegate
+                         >
+
+- (void)getContactsAsync:(CDVInvokedUrlCommand*)command;
+
+@end
